8af4202cc19f28dafba9d2a488b50a8359f978e9
aniydx <aniydx@gmail.com>
2024-04-22 08:36:04 +0800

    1

--- config/config.go
+++ config/config.go
@@ -1,6 +1,6 @@
 package config
 
-const webHookAlert = "https://oapi.dingtalk.com/robot/send?access_token=614f6b6d7f91fb77de01e012df3b17751c7fd125d06fe4af482b562e3bda3b97"
+const webHookAlert = "https://oapi.dingtalk.com/robot/send?access_token=123"
 
 type DingTalkMsg struct {
 	MsgType  string `json:"msgtype"`
